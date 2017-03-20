235be0ad518d006c0721d415d22835bb64d2bb13
Ryo-chin <h031203yama@gmail.com>
2017-03-20 17:24:00 +0900

    STEP0_explore_java_networking_package #comment ソケット & 使いそうなパッケージ調査 完了

---
+++ src/main/java/HttpServer.java
@@ -0,0 +1,13 @@
+import java.net.Socket;
+
+/**
+ * JavaによるシンプルなWebサーバー
+ * {@link Socket} http://docs.oracle.com/javase/jp/6/api/java/net/Socket.html
+ *
+ * @author ryohei.yamamoto
+ */
+public class HttpServer {
+
+    public static void main(String[] args) {
+    }
+}
